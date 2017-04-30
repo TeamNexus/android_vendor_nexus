c8a31dbecec4bc7954d5d0beda2214a7978c6258
Lukas0610 <mail@lukasberger.at>
2017-04-30 23:57:17 +0200

    vendor: corrected source-file in makefile

--- packages/apps/TheNexus/Android.mk
+++ packages/apps/TheNexus/Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE               := TheNexus-prebuilt
-LOCAL_SRC_FILES            := $(LOCAL_MODULE).apk
+LOCAL_SRC_FILES            := TheNexus.apk
 LOCAL_MODULE_TAGS          := optional
 LOCAL_MODULE_CLASS         := APPS
 LOCAL_MODULE_SUFFIX        := $(COMMON_ANDROID_PACKAGE_SUFFIX)
