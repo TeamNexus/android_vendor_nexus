d457cbec98662cc6a242ae67dcf7e72aefe4bf30
Lukas0610 <mail@lukasberger.at>
2017-04-30 10:47:47 +0200

    vendor: updated and added makefiles

--- packages/apps/Android.mk
+++ packages/apps/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 TeamNexus
+# Copyright (C) 2017 Team Nexus7420
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,14 +15,4 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE               := HTCCamera
-LOCAL_SRC_FILES            := $(LOCAL_MODULE).apk
-LOCAL_MODULE_TAGS          := optional
-LOCAL_MODULE_CLASS         := APPS
-LOCAL_MODULE_SUFFIX        := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE          := PRESIGNED
-LOCAL_OVERRIDES_PACKAGES   := Camera2
-LOCAL_PRIVILEGED_MODULE    := true
-include $(BUILD_PREBUILT)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
--- packages/apps/TheNexus/Android.mk
+++ packages/apps/TheNexus/Android.mk
@@ -19,6 +19,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE               := TheNexus
 LOCAL_SRC_FILES            := $(LOCAL_MODULE).apk
+LOCAL_MODULE_TAGS          := optional
 LOCAL_MODULE_CLASS         := APPS
 LOCAL_MODULE_SUFFIX        := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE          := PRESIGNED
