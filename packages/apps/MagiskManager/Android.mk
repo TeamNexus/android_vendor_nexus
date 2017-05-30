30bfcc5b20dbcd9c2612bb49a6630a3df6b66473
Lukas0610 <mail@lukasberger.at>
2017-05-30 11:26:51 +0200

    vendor: added NexusLauncher, updated makefiles

--- packages/apps/MagiskManager/Android.mk
+++ packages/apps/MagiskManager/Android.mk
@@ -17,12 +17,13 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := MagiskManager-prebuilt
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := APPS
-LOCAL_BUILT_MODULE_STEM := package.apk
-# Make sure the build system doesn't try to resign the APK
-LOCAL_CERTIFICATE       := PRESIGNED
-LOCAL_DEX_PREOPT        := false
-LOCAL_SRC_FILES         := MagiskManager.apk
+LOCAL_MODULE               := MagiskManager-prebuilt
+LOCAL_SRC_FILES            := $(LOCAL_MODULE).apk
+LOCAL_MODULE_TAGS          := optional
+LOCAL_MODULE_CLASS         := APPS
+LOCAL_MODULE_SUFFIX        := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_PRIVILEGED_MODULE    := true
+LOCAL_BUILT_MODULE_STEM    := package.apk
+LOCAL_CERTIFICATE          := PRESIGNED
+LOCAL_DEX_PREOPT           := false
 include $(BUILD_PREBUILT)
--- packages/apps/NexusLauncher/Android.mk
+++ packages/apps/NexusLauncher/Android.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE               := HTCCamera
+LOCAL_MODULE               := NexusLauncher
 LOCAL_SRC_FILES            := $(LOCAL_MODULE).apk
 LOCAL_MODULE_TAGS          := optional
 LOCAL_MODULE_CLASS         := APPS
@@ -26,5 +26,5 @@ LOCAL_PRIVILEGED_MODULE    := true
 LOCAL_BUILT_MODULE_STEM    := package.apk
 LOCAL_CERTIFICATE          := PRESIGNED
 LOCAL_DEX_PREOPT           := false
-LOCAL_OVERRIDES_PACKAGES   := Camera2
+LOCAL_OVERRIDES_PACKAGES   := Trebuchet
 include $(BUILD_PREBUILT)
